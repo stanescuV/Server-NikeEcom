a6970e3cbeaff615791dc5232ac9e33941e8e29a
Stanescu Victor <stanescuvictor12@gmail.com>
2023-08-13 14:22:37 +0200

    Trying to make Date universale !

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const sql = require("mssql");
 const sqlConfig = {
   user: "test1",
   password: "1234",
-  database: "test 1234",
+  database: "TEST_DB",
   server: "LAPTOP-GFHOUTJ9\\SQLEXPRESS",
   pool: {
     max: 10,
@@ -18,11 +18,21 @@ const start = async () => {
   try {
     // make sure that any items are correctly URL encoded in the connection string
     await sql.connect(sqlConfig);
-    const result = await sql.query`select 2`;
-    console.dir(result);
+    return sql;
+
+    //QUERY DE INSERT PRODUCTS
+    // for (let i = 0; i < data.length; i++) {
+    //   let y = data[i];
+    //   // let queryInsert = `INSERT Products (marca, nume, pret) SELECT '${y.marca}', '${y.name}', ${y.pret}`;
+    //   // const result = await sql.query(queryInsert);
+    //   console.dir(result);
+    // }
   } catch (err) {
     console.log(err);
   }
 };
 
 start();
+
+//export in NODE JS
+module.exports = { start };
